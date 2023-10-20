53d567853a3aae07321bae47396983f2d9918c78
noodlechoi <choime321@gmail.com>
2023-10-20 23:56:41 +0900

    make Rail class

--- code/Start_Menu.py
+++ code/Start_Menu.py
@@ -1,16 +1,27 @@
 from pico2d import *
 
+class Rail():
+    rail_img = None
+    def __init__(self):
+        if Rail.rail_img == None:
+            Rail.rail_img = load_image('../resource/bowling_rail.png')
 
+    def draw(self):
+        # draw rail : WIDTH // 4 and HEIGHT // 2
+        Rail.rail_img.clip_draw(0, 0, 85, 208, WIDTH // 4, HEIGHT // 2, WIDTH // 2, HEIGHT)
+        Rail.rail_img.clip_composite_draw(0, 0, 85, 208, 0, 'h', WIDTH // 4 * 3, HEIGHT // 2, WIDTH // 2, HEIGHT)
 
 open_canvas()
+
+# get screen size
+WIDTH = get_canvas_width()
+HEIGHT = get_canvas_height()
+
 while True:
     clear_canvas()
-    rail_img = load_image('../resource/bowling_rail.png')
-    WIDTH = get_canvas_width()
-    HEIGHT = get_canvas_height()
-    # draw rail, WIDTH // 4 and HEIGHT // 2
-    rail_img.clip_draw(0, 0, 85, 208, WIDTH // 4, HEIGHT // 2, WIDTH // 2, HEIGHT)
-    rail_img.clip_composite_draw(0, 0, 85, 208, 0, 'h', WIDTH // 4 * 3, HEIGHT // 2, WIDTH // 2, HEIGHT)
+
+    rail = Rail()
+    rail.draw()
 
     update_canvas()
     delay(0.1)
