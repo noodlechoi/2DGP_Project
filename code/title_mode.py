ff3aa407241d0fe29f554613456fa71f3d73b246
noodlechoi <choime321@gmail.com>
2023-11-04 20:14:25 +0900

    draw title name and start exit text

--- code/title_mode.py
+++ code/title_mode.py
@@ -7,10 +7,10 @@ from rail import Rail
 from game_world import WIDTH, HEIGHT
 
 PIXEL_PER_METER = (10.0 / 0.3)
-RUN_SPEED_KMPH = 20.0
-RUN_SPEED_MPM = RUN_SPEED_KMPH * 1000.0 / 60.0
-RUN_SPEED_MPS = RUN_SPEED_MPM / 60.0
-RUN_SPEED_PPS = RUN_SPEED_MPS * PIXEL_PER_METER
+MOVE_SPEED_KMPH = 20.0
+MOVE_SPEED_MPM = MOVE_SPEED_KMPH * 1000.0 / 60.0
+MOVE_SPEED_MPS = MOVE_SPEED_MPM / 60.0
+MOVE_SPEED_PPS = MOVE_SPEED_MPS * PIXEL_PER_METER
 
 # 시간 당 프레임
 TIME_PER_ACTION = 1.0
@@ -19,8 +19,11 @@ FRAMES_PER_ACTION = 5
 
 FRAMES_PER_TIME = ACTION_PER_TIME * FRAMES_PER_ACTION
 
+# 소닉 로고, 제목, 소닉 애니메이션, start_exit
+location = [[225, 80], [470, 260], [450, 468], [460, 205]]
+
 def init():
-    global image
+    global sonic_img, title_img, text_img
     global running
     global rail
     global x, y, padding
@@ -36,12 +39,17 @@ def init():
     rail = Rail()
     running = True
     # 소닉 이미지
-    image = load_image('../resource/title_sonic(1).png')
+    sonic_img = load_image('../resource/title_sonic(1).png')
+
+    title_img = load_image('../resource/title_name.png')
+    text_img = load_image('../resource/start_exit(1).png')
+
 
 def finish():
-    global image, rail
-    del image
+    global sonic_img, rail
+    del sonic_img
     del rail
+    del location
 
 def update():
     pass
@@ -64,12 +72,15 @@ def draw():
     rail.draw()
     # title
     # width : 260, height : 155
-    image.clip_draw(225, 80, 260, 155, WIDTH // 2, HEIGHT // 3 * 2, WIDTH // 2, HEIGHT // 2)
+    sonic_img.clip_draw(location[0][0], location[0][1], 260, 155, WIDTH // 2, HEIGHT // 3 * 2, WIDTH // 2, HEIGHT // 2)
+    title_img.draw(location[2][0], location[2][1], 250, 80)
 
     # index 2~ : start : 675, x = 110
     # 프레임 = 한 액션 당 프레임 수 * 시간 당 액션 수 * 프레임 시간
     frame = (frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % FRAMES_PER_ACTION
-    image.clip_draw(470 + (int(frame) * x) + padding, 260, x, y, WIDTH // 2, HEIGHT // 4 * 3)
+    sonic_img.clip_draw(location[1][0] + (int(frame) * x) + padding, location[1][1], x, y, WIDTH // 2, HEIGHT // 4 * 3)
+
+    text_img.draw(location[3][0], location[3][1], 300, 200)
     update_canvas()
 
 def handle_events():
